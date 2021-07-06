eb4ca0ccca96bebad7624040fc57a2d9f4548eef
Ilona <ilonarus@hotmail.com>
2021-07-06 11:16:00 +0200

    Removed unused id creator

--- src/components/AddTask.vue
+++ src/components/AddTask.vue
@@ -35,7 +35,6 @@
           return
         }
         const newTask = {
-          id: Math.floor(Math.random() * 100000),
           text: this.text,
           day: this.day,
           reminder: this.reminder
