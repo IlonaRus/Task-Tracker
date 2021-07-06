a429eb7775fba2584972254ef3a4642470832896
Ilona <ilonarus@hotmail.com>
2021-07-06 11:17:28 +0200

    Added calls for delete, add and toggle task/reminder

--- src/App.vue
+++ src/App.vue
@@ -1,118 +1,154 @@
 <template>
   <div class="container">
-    <Header 
-      @toggle-add-task="toggleAddTask" 
-      title="Task Tracker" 
-      :showAddTask="showAddTask" />
+    <Header
+      @toggle-add-task="toggleAddTask"
+      title="Task Tracker"
+      :showAddTask="showAddTask"
+    />
     <div v-show="showAddTask">
       <AddTask @add-task="addTask" />
     </div>
-  
-    <Tasks 
-      @toggle-reminder="toggleReminder" 
+
+    <Tasks
+      @toggle-reminder="toggleReminder"
       @delete-task="deleteTask"
-      :tasks="tasks" />
+      :tasks="tasks"
+    />
   </div>
 </template>
 
 <script>
-import Header from './components/Header'
-import Tasks from './components/Tasks'
-import AddTask from './components/AddTask'
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
 
-  export default {
-    name: "App",
-    components: {
-      Header,
-      Tasks,
-      AddTask
+export default {
+  name: "App",
+  components: {
+    Header,
+    Tasks,
+    AddTask,
+  },
+  data() {
+    return {
+      tasks: [],
+      showAddTask: false,
+    };
+  },
+  methods: {
+    toggleAddTask() {
+      this.showAddTask = !this.showAddTask;
     },
-    data() {
-      return {
-        tasks: [],
-        showAddTask: false
+
+    async addTask(task) {
+      const response = await fetch("api/tasks", {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(task),
+      });
+
+      const data = await response.json();
+      this.tasks = [...this.tasks, data];
+    },
+
+    async deleteTask(id) {
+      if (confirm("Are you sure?")) {
+        const response = await fetch(`api/tasks/${id}`, {
+          method: "DELETE",
+        });
+
+        response.status === 200
+          ? (this.tasks = this.tasks.filter((task) => task.id !== id))
+          : alert("Error deleting task");
       }
     },
-    methods: {
-      toggleAddTask() {
-        this.showAddTask = !this.showAddTask
-      },
 
-      addTask(task) {
-        this.tasks = [...this.tasks, task]
-      },
-
-      deleteTask(id) {
-        if(confirm('Are you sure?')) {
-          this.tasks = this.tasks.filter((task) => task.id !== id)
-        }
-      },
-
-      toggleReminder(id) {
-        this.tasks = this.tasks.map((task) => 
-        task.id === id ? { ...task, reminder: !task.reminder} : task)
-      },
-
-      async fetchTasks() {
-        const respose = await fetch('http://localhost:5000/tasks')
-        const data = await respose.json()
-
-        return data
-      }
+    async toggleReminder(id) {
+      const taskToToggle = await this.fetchTask(id)
+      const updatedTask = {...taskToToggle, reminder: !taskToToggle.reminder}
+      const response = await fetch(`api/tasks/${id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-type': 'application/json'
+        },
+        body: JSON.stringify(updatedTask)
+      })
+      const data = await response.json()
+      
+      this.tasks = this.tasks.map((task) =>
+        task.id === id ? { ...task, reminder: data.reminder } : task
+      );
     },
-    async created() {
-      this.tasks = await this.fetchTasks()
-    }
-  };
+
+    async fetchTasks() {
+      const respose = await fetch("api/tasks");
+      const data = await respose.json();
+
+      return data;
+    },
+
+    async fetchTask(id) {
+      const respose = await fetch(`api/tasks/${id}`);
+      const data = await respose.json();
+
+      return data;
+    },
+  },
+  async created() {
+    this.tasks = await this.fetchTasks();
+  },
+};
 </script>
 
 <style>
-  @import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap");
 
-  * {
-    box-sizing: border-box;
-    margin: 0;
-    padding: 0;
-  }
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
 
-  body {
-    font-family: "Poppins", sans-serif;
-  }
+body {
+  font-family: "Poppins", sans-serif;
+}
 
-  .container {
-    max-width: 500px;
-    margin: 30px auto;
-    overflow: auto;
-    min-height: 300px;
-    border: 1px solid steelblue;
-    padding: 30px;
-    border-radius: 5px;
-  }
+.container {
+  max-width: 500px;
+  margin: 30px auto;
+  overflow: auto;
+  min-height: 300px;
+  border: 1px solid steelblue;
+  padding: 30px;
+  border-radius: 5px;
+}
 
-  .btn {
-    display: inline-block;
-    background: #000;
-    color: #fff;
-    border: none;
-    padding: 10px 20px;
-    margin: 5px;
-    border-radius: 5px;
-    cursor: pointer;
-    text-decoration: none;
-    font-size: 15px;
-    font-family: inherit;
-  }
+.btn {
+  display: inline-block;
+  background: #000;
+  color: #fff;
+  border: none;
+  padding: 10px 20px;
+  margin: 5px;
+  border-radius: 5px;
+  cursor: pointer;
+  text-decoration: none;
+  font-size: 15px;
+  font-family: inherit;
+}
 
-  .btn:focus {
-    outline: none;
-  }
+.btn:focus {
+  outline: none;
+}
 
-  .btn:active {
-    transform: scale(0.98);
-  }
+.btn:active {
+  transform: scale(0.98);
+}
 
-  .btn-block {
-    display: block;
-    width: 100%;
-  }
+.btn-block {
+  display: block;
+  width: 100%;
+}
 </style>
