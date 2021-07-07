d84235883bf4440156eedde22781dee26ebbbc86
Ilona <ilonarus@hotmail.com>
2021-07-07 16:16:34 +0200

    Hide task button on about page

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,8 @@
 <template>
   <header>
     <h1>{{ title }}</h1>
-    <Button
+    <Button 
+      v-show="homePage"
       @btn-click="$emit('toggle-add-task')"
       :text="showAddTask ? 'Close' : 'Add Task'"
       :color="showAddTask ? 'red' : 'green'"
@@ -11,25 +12,34 @@
 
 
 <script>
-  import Button from './Button'
- 
- export default {
-    name: 'Header',
-    props: {
-      title: String,
-      showAddTask: Boolean
-    },
-    components: {
-      Button
+import Button from "./Button";
+
+export default {
+  name: "Header",
+  props: {
+    title: String,
+    showAddTask: Boolean,
+  },
+  components: {
+    Button,
+  },
+  computed: {
+    homePage() {
+      if(this.$route.path === '/') {
+        return true
+      } else {
+        return false
+      }
     }
   }
+};
 </script>
 
 <style scoped>
-  header {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    margin-bottom: 20px;
-  }
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
 </style>
