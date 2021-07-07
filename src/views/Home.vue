f181db81d58441ec7d2b68860d7abcdb3ca3c6ac
Ilona <ilonarus@hotmail.com>
2021-07-07 16:17:26 +0200

    Removed unused div with v-show on home-page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,5 @@
 <template>
-  <div v-show="showAddTask">
-    <AddTask @add-task="addTask" />
-  </div>
-
+  <AddTask v-show="showAddTask" @add-task="addTask" />
   <Tasks
     @toggle-reminder="toggleReminder"
     @delete-task="deleteTask"
@@ -27,8 +24,8 @@ export default {
   data() {
     return {
       tasks: [],
-    }
-  }, 
+    };
+  },
 
   methods: {
     async addTask(task) {
